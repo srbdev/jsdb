fba0fd6d8f8d4800c311b2d0cf1f70a14ab0a849
srbdev <srbdevelopment@gmail.com>
2016-01-05 19:14:14 -0700

    Add stub functions

--- jsdb.js
+++ jsdb.js
@@ -1,4 +1,8 @@
 
 /* jshint esnext: true, asi: true */
 
-exports.print = () => console.log('Hello from jsdb!')
+exports.createDatabase = (name) => console.log(`Create database ${name}`)
+exports.createTable = (name) => console.log(`Create table ${name}`)
+exports.useDatabase = (name) => console.log(`Use database ${name}`)
+exports.saveDatabase = (url) => console.log(`Save database in ${url}`)
+exports.loadDatabase = (url) => console.log(`Load database from ${url}`)
